44277e25b4fc24f59a091455f791d4cedb82975f
Harry <crayoncubeone@gmail.com>
2023-05-18 10:20:29 +0530

    grid size working

--- index.js
+++ index.js
@@ -1,43 +1,86 @@
-const grid = document.querySelector('.grid');
-R =  `${Math.floor(Math.random()*255)}`
-G =  `${Math.floor(Math.random()*255)}`
-B =  `${Math.floor(Math.random()*255)}`
+//Instantiating grid  and random RGB values
+const grid = document.querySelector(".grid");
+R = `${Math.floor(Math.random() * 255)}`;
+G = `${Math.floor(Math.random() * 255)}`;
+B = `${Math.floor(Math.random() * 255)}`;
 
-const randomColor = 'rgb(' +R+","+G+","+B+")"
+const randomColor = "rgb(" + R + "," + G + "," + B + ")";
+// const size = document.getElementById('gridSubmit').addEventListener('submi',function(){
+//     console.log("size")
+// })
 
 createGrid = () => {
-for (let i = 0; i < 256; i++) {
-    const div = document.createElement('div');
-    div.classList.add('square');
-    div.addEventListener('mouseover', function(event){
-        event.target.style.backgroundColor = randomColor;
-    })
-    grid.appendChild(div); 
-}
+  // if(!slider){
+
+  for (let i = 0; i < 256; i++) {
+    const div = document.createElement("div");
+    div.classList.add("square");
+    div.addEventListener("mouseover", function (event) {
+      event.target.style.backgroundColor = randomColor;
+    });
+    grid.appendChild(div);
+  }
+  // } else{
+//   console.log("slider");
+//   for (let i = 0; i < slider.value * slider.value; i++) {
+//     const div = document.createElement("div");
+//     div.classList.add("square");
+//     div.addEventListener("mouseover", function (event) {
+//       event.target.style.backgroundColor = randomColor;
+//     });
+//     grid.appendChild(div);
+//   }
+  // }
 };
 
-    console.log('reset')
-    resetbutton = document.querySelector('#reset');
+// Slider
+const slider = document.getElementById("SliderRange");
+const output = document.getElementById("sizeValue");
 
-    resetbutton.addEventListener('click',function(){
-        const cells = document.getElementsByClassName('square')
-        
-        for (let i = 0; i < cells.length; i++) {
-            cells[i].style.backgroundColor='white';
-            
-        }
+output.innerHTML = slider.value;
 
-        console.log("reset ")
-    })
+function removeAllChildNodes(parent){
+    while (parent.firstChild) {
+            parent.removeChild(parent.firstChild);
+    }
+}
 
+slider.addEventListener('input',function () {
+        let val =  document.getElementById('SliderRange').value;
+        output.textContent = val;
+        removeAllChildNodes(grid);
+        grid.setAttribute('style',`grid-template-columns:repeat(${val},2fr); grid-template-rows:repeat(${val},2fr);`);
+        for (let i = 0; i < val * val; i++) {
+            const div = document.createElement("div");
+            div.classList.add("square");
+            div.addEventListener("mouseover", function (event) {
+              event.target.style.backgroundColor = randomColor;
+            });
+            grid.appendChild(div);
+          }
+})
 
-   const color = document.querySelector('#color');
-    color.addEventListener('click',function(){
-       alert("this feature is not free , refresh and get a new color") 
-       
-        console.log("color change" + randomColor);
-        
-    });
+// Reset
+console.log("reset");
+resetbutton = document.querySelector("#reset");
 
+resetbutton.addEventListener("click", function () {
+  const cells = document.getElementsByClassName("square");
 
+  for (let i = 0; i < cells.length; i++) {
+    cells[i].style.backgroundColor = "white";
+  }
+
+  console.log("reset ");
+});
+
+// Color
+const color = document.querySelector("#color");
+color.addEventListener("click", function () {
+  alert("this feature is not free , refresh and get a new color");
+
+  console.log("color change" + randomColor);
+});
+
+//Calling create grid as soon as the page loads
 createGrid();
